304499d682533f33355f1484b17e2f5d09ee1179
Kadu Franca <kadu.franca@projuris.com.br>
2023-04-23 14:03:14 -0300

    corrigido lista de balanceados

--- arena/arena.js
+++ arena/arena.js
@@ -1,18 +1,19 @@
+let id = 1;
+let balanceados = [
+  17, 12, 25, 27, 39, 74, 35, 41, 52, 54, 56, 58, 63, 72, 60, 50, 84, 86, 88,
+  109, 90, 98, 100, 102, 109,
+];
+let random = balanceados[Math.floor(Math.random() * balanceados.length + 1)];
 
+const getplayerPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+const getashPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${random}`;
 
-let id = 1
-let balanceados = [34, 38, 62, 65, 68, 71, 76, 78, 80, 82, 85, 87, 97, 99, 101, 103, 105, 106, 107, 108, 112, 113, 114, 121, 135]
-let random = balanceados[Math.floor(Math.random() * balanceados.length + 1)]
-
-const getplayerPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}`
-const getashPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${random}`
-
-let playerPokemon = {}
-let ashPokemon = {}
+let playerPokemon = {};
+let ashPokemon = {};
 
 fetch(getplayerPokemonUrl)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     playerPokemon = {
       id: id,
       name: data.forms[0].name,
@@ -22,15 +23,13 @@ fetch(getplayerPokemonUrl)
       specialAttk: data.stats[3].base_stat,
       specialDefense: data.stats[4].base_stat,
       speed: data.stats[5].base_stat,
-      sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`
-    }
-  }
-);
-
+      sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`,
+    };
+  });
 
 fetch(getashPokemonUrl)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     ashPokemon = {
       id: random,
       name: data.forms[0].name,
@@ -40,16 +39,9 @@ fetch(getashPokemonUrl)
       specialAttk: data.stats[3].base_stat,
       specialDefense: data.stats[4].base_stat,
       speed: data.stats[5].base_stat,
-      sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${random}.png`
-    }
-  }
-);
-
-
-
-
-
-
+      sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${random}.png`,
+    };
+  });
 
 function choseAction() {
   const div = document.getElementById("container");
@@ -59,10 +51,9 @@ function choseAction() {
   var imgNew2 = document.createElement("img");
   imgNew2.src = ashPokemon.sprite;
 
-  div.appendChild(imgNew)
-  div.appendChild(imgNew2)
+  div.appendChild(imgNew);
+  div.appendChild(imgNew2);
 
-  console.log(playerPokemon)
-  console.log(ashPokemon)
+  console.log(playerPokemon);
+  console.log(ashPokemon);
 }
-
